840fd512adfabb7f04a26e8fef3dfc0a7fe9a563
Tripp Uroskie <52885962+trippuroskie@users.noreply.github.com>
2025-01-14 07:17:03 -0700

    Update main_streamlit.py - modify path to bg image

--- main_streamlit.py
+++ main_streamlit.py
@@ -250,7 +250,7 @@ def add_bg_from_local(image_file):
     )
 
 # Add the background image
-add_bg_from_local('brighton.png')
+add_bg_from_local('brighton-bot/brighton.png')
 
 # Add custom CSS for the container
 st.markdown("""
